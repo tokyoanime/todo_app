4c815c00f49a9a90247ba6849637dd615e1cfaea
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 00:14:18 -0700

    populate all TodoItem for index

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -3,13 +3,18 @@ class Api::V1::TodoItemsController < ApplicationController
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    @todo_items = TodoItem.all
   end
+
   def show
   end
+
   def create
   end
+
   def update
   end
+  
   def destroy
   end
 

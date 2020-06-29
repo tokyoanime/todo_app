136558ba4427888b38afe1d0d8ab72f5af6a8956
Lwin Ye <lwin_ye@yahoo.com>
2020-06-28 23:50:46 -0700

    add desc order default scope to TodoItem model

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,7 @@
 class TodoItem < ApplicationRecord
+  # ensure that the newest todo item appear first when queried
+  default_scope { order(created_at: :desc) }
+
   belongs_to :user
 
   validates :title, presence: true

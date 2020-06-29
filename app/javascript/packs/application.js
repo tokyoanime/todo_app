2d95a9c615ee7c7eedaef7d670fb2673927f91d7
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 01:00:14 -0700

    initial basic react setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,6 @@ require('channels');
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require('./hello_react');
+require('./components/TodoApp');
 require('bootstrap');
 import 'bootstrap/dist/css/bootstrap';
---
+++ app/javascript/packs/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TodoApp extends React.Component {
+  render() {
+    return <p>TodoApp</p>;
+  }
+}
+
+document.addEventListener('turbolinks:load', () => {
+  const app = document.getElementById('todo-app');
+  app && ReactDOM.render(<TodoApp />, app);
+});

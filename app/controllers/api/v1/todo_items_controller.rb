4af3537b331ec94878c966d6e051b92b8c400122
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 00:19:09 -0700

    authenticate user for TodoItemsController

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::TodoItemsController < ApplicationController
+  # authenticate user using devise buildin method before user can access these routes
+  before_action :authenticate_user!
+
   # find todo item by ID for show, edit, update and destory route
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
 
@@ -14,12 +17,16 @@ class Api::V1::TodoItemsController < ApplicationController
 
   def update
   end
-  
+
   def destroy
   end
 
   private
   def set_todo_item
-      @todo_item = TodoItem.find(params[:id])
+    @todo_item = TodoItem.find(params[:id])
+  end
+
+  def authorized?
+    @todo_item.user == current_user
   end
 end

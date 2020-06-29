ebd721c6b12cdd340352516b3df0b7894fd0c5d4
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 01:33:54 -0700

    todo form component

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import TodoItems from './TodoItems';
+import TodoItem from './TodoItem';
 
 class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: [],
+    };
+
+    this.getTodoItems = this.getTodoItems.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTodoItems();
+  }
+
+  getTodoItems() {
+    axios
+      .get('/api/v1/todo_items')
+      .then((res) => {
+        const todoItems = res.data;
+        this.setState({ todoItems });
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
-    return <p>TodoApp</p>;
+    return (
+      <TodoItems>
+        {this.state.todoItems.map((todoItem) => (
+          <TodoItem key={todoItem.id} todoItem={todoItem} />
+        ))}
+      </TodoItems>
+    );
   }
 }
 
---
+++ app/javascript/packs/components/TodoForm.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import TodoItem from './TodoItem';
+
+export default class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.titleRef = React.createRef();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post('/api/v1/todo_items', {
+        todo_item: {
+          title: this.titleRef.current.value,
+          complete: false,
+        },
+      })
+      .then((res) => {
+        const todoItem = res.data;
+        this.props.createTodoItem(todoItem);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    e.target.reset();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className='my-3'>
+        <div className='form-row'>
+          <div className='form-group col-md-8'>
+            <input
+              type='text'
+              name='title'
+              ref={this.titleRef}
+              required
+              className='form-control'
+              id='title'
+              placeholder='Write your todo item here ...'
+            />
+          </div>
+          <div className='form-group col-md-4'>
+            <button className='btn btn-outline-success btn-block'>
+              Add To Do Item
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+TodoItem.PropTypes = {
+  createTodoItem: PropTypes.func.isRequired,
+};

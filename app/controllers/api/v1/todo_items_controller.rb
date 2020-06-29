42c1b0fec2e6b1a6aeb4066d6980edb89a25ba45
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 00:01:58 -0700

    initial todo item controller

---
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::TodoItemsController < ApplicationController
+  # find todo item by ID for show, edit, update and destory route
+  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+  def show
+  end
+  def create
+  end
+  def update
+  end
+  def destroy
+  end
+
+  private
+  def set_todo_item
+      @todo_item = TodoItem.find(params[:id])
+  end
+end

1d30d9ca0f51fd289c8a6cdfb15a572b469b672c
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 00:46:25 -0700

    todo item create controller

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -22,6 +22,18 @@ class Api::V1::TodoItemsController < ApplicationController
   end
 
   def create
+    @todo_item = current_user.todo_items.build(todo_item_params)
+    if authorized?
+      respond_to do |format|
+        if @todo_item.save
+          format.json { render :show, status: :created, location: api_v1_todo_item_path(@todo_item)}
+        else
+          format.json { render json: @todo_item.errors.full_messages, status: :unprocessable_entity}
+        end
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def update
@@ -45,5 +57,9 @@ class Api::V1::TodoItemsController < ApplicationController
         format.json { render :unauthorized, status: 401 }
       end
     end
+  end
+
+  def todo_item_params
+    params.require(:todo_item).permit(:title, :complete)
   end
 end

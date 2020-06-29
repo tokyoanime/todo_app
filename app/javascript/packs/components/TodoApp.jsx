9e4a2cae8bcc9f2665f043fd96048e3237e8e251
Lwin Ye <lwin_ye@yahoo.com>
2020-06-29 11:51:18 -0700

    error handling component

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -6,6 +6,7 @@ import TodoItems from './TodoItems';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
 import Spinner from './Spinner';
+import ErrorMessage from './ErrorMessage';
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -14,11 +15,14 @@ class TodoApp extends React.Component {
       todoItems: [],
       hideCompletedTodoItems: false,
       isLoading: true,
+      errorMessage: null,
     };
 
     this.getTodoItems = this.getTodoItems.bind(this);
     this.createTodoItem = this.createTodoItem.bind(this);
     this.toggleCompletedTodoItems = this.toggleCompletedTodoItems.bind(this);
+    this.handleErrors = this.handleErrors.bind(this);
+    this.clearErrors = this.clearErrors.bind(this);
   }
 
   componentDidMount() {
@@ -51,12 +55,27 @@ class TodoApp extends React.Component {
     });
   }
 
+  handleErrors(errorMessage) {
+    this.setState({ errorMessage });
+  }
+
+  clearErrors() {
+    this.setState({ errorMessage: null });
+  }
+
   render() {
     return (
       <>
+        {this.state.errorMessage && (
+          <ErrorMessage errorMessage={this.state.errorMessage} />
+        )}
         {!this.state.isLoading && (
           <>
-            <TodoForm createTodoItem={this.createTodoItem} />
+            <TodoForm
+              createTodoItem={this.createTodoItem}
+              handleErrors={this.handleErrors}
+              clearErrors={this.clearErrors}
+            />
             <TodoItems
               toggleCompletedTodoItems={this.toggleCompletedTodoItems}
               hideCompletedTodoItems={this.state.hideCompletedTodoItems}
